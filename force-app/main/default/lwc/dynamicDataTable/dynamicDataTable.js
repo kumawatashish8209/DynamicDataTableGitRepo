cd433bcb2c6ba045f8d5928eb3048f40e0852b33
Ashish Kumawat <kumawatashish@concret.io>
2025-09-08 18:38:35 +0530

    add logic for get data according to object

--- force-app/main/default/lwc/dynamicDataTable/dynamicDataTable.js
+++ force-app/main/default/lwc/dynamicDataTable/dynamicDataTable.js
@@ -1,11 +1,16 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import getListOfObjects from '@salesforce/apex/DataTableHandler.getListOfObjects';
+import getRecords from '@salesforce/apex/DataTableHandler.getRecords';
 
 export default class DynamicDataTable extends LightningElement {
     @track selectedValue = '';
     @track options = [];
     @track records = [];
+    @track columns = [];
+    @track drafvals = [];
+    @track tableVisibility = false;
     apiToObj = {};
+    error;
 
     
     connectedCallback() {
@@ -30,5 +35,28 @@ export default class DynamicDataTable extends LightningElement {
 
     handleChangeSelectedObject(event) {            //handle event when object is selected
         this.selectedValue = event.detail.value;
+        this.tableVisibility = true;
+    }
+
+    @wire(getRecords,{objectName: this.selectedValue}) 
+    handleWiredRecords(result) {
+        if(result.data) {
+            this.records = data;
+            this.columns = this.getColumns(data);
+        }else {
+            this.error = result.error;
+            console.log('Error: ' + this.error);
+        }
+    }
+
+    getColumns(data) {
+        let columns = [];
+        data.forEach(record => {
+            Object.keys(record).forEach(key => {
+                columns.push({label: key, fieldName: key});
+            });
+        });
+        
+        return columns;
     }
 }
