a5e53abe168518ed96f6e1036efda4907150cc28
Ashish Kumawat <kumawatashish@concret.io>
2025-09-08 14:52:22 +0530

    add targets

--- force-app/main/default/lwc/dynamicDataTable/dynamicDataTable.js
+++ force-app/main/default/lwc/dynamicDataTable/dynamicDataTable.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class DynamicDataTable extends LightningElement {}
+export default class DynamicDataTable extends LightningElement {
+    
+}
