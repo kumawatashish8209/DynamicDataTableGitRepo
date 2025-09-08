7b40d73d72d1fcec0b908fa095cabf399226ebde
Ashish Kumawat <kumawatashish@concret.io>
2025-09-08 16:29:48 +0530

    add search list for objects

--- force-app/main/default/lwc/dynamicDataTable/dynamicDataTable.js
+++ force-app/main/default/lwc/dynamicDataTable/dynamicDataTable.js
@@ -1,5 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import getListOfObjects from '@salesforce/apex/DataTableHandler.getListOfObjects';
 
 export default class DynamicDataTable extends LightningElement {
+    @track selectedValue = '';
+    @track options = [];
+
     
+    connectedCallback() {
+        getListOfObjects()
+        .then(result => {
+            if (Array.isArray(result)) {
+                this.options = result.map(obj => ({ label: obj, value: obj }));  // format the options
+            } else {
+                this.options = [];
+            }
+        })
+        .catch(error => {
+            this.options = [];
+            console.log('Error: ' + error);
+        } );
+    }
+
+    handleChange(event) {
+        this.selectedValue = event.detail.value;
+    }
 }
