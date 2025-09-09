0723f5da18a6c970df8920e5a12037d15d731c14
Ashish Kumawat <kumawatashish@concret.io>
2025-09-09 15:23:42 +0530

    Complete logic for dynamically get records of particular object according to related fields

--- force-app/main/default/lwc/dynamicDataTable/dynamicDataTable.js
+++ force-app/main/default/lwc/dynamicDataTable/dynamicDataTable.js
@@ -1,15 +1,19 @@
-import { LightningElement, track, wire } from 'lwc';
+import { LightningElement, track} from 'lwc';
 import getListOfObjects from '@salesforce/apex/DataTableHandler.getListOfObjects';
-import getRecords from '@salesforce/apex/DataTableHandler.getRecords';
+import getFields from '@salesforce/apex/DataTableHandler.getFields';
+import getData from '@salesforce/apex/DataTableHandler.getData';
 
 export default class DynamicDataTable extends LightningElement {
-    @track selectedValue = '';
-    @track options = [];
+    @track selectedValue;
+    @track objectOptions = [];
     @track records = [];
     @track columns = [];
     @track drafvals = [];
+    @track fieldsOptions = [];
+    @track selectedFields = [];
+    @track visibleFields = [];
+    @track fieldsVisibility = false;
     @track tableVisibility = false;
-    apiToObj = {};
     error;
 
     
@@ -17,46 +21,83 @@ export default class DynamicDataTable extends LightningElement {
         //get all objects from org
         getListOfObjects()
         .then(result => {
-            this.apiToObj = result;
-            this.options = Object.entries(this.apiToObj)
-                     .map(([key, value]) => ({ label: value, value: key }))
+            let apiToObj = result;
+            this.objectOptions = Object.entries(apiToObj)
+                     .map(([key, value]) => ({ label: key, value: value }))
                      .sort((a, b) => {
                          // Compare labels alphabetically
                          if(a.label < b.label) return -1;
                          if(a.label > b.label) return 1;
                          return 0;
                      });
+
         })
         .catch(error => {
-            this.options = [];
-            console.log('Error: ' + error);
+            this.objectOptions = [];
+            this.showToast('Error getting objects', error.body.message, 'error');
+            //console.log('Error while getting all objects: ' + error);
         } );
     }
 
     handleChangeSelectedObject(event) {            //handle event when object is selected
         this.selectedValue = event.detail.value;
+        this.getObjectFields();
+        this.fieldsVisibility = true;
+    }
+
+    handleCancel() {
+        this.fieldsVisibility = false;
+    }
+
+    handleLoad() {
+        this.setColoumns(this.selectedFields);
+        this.loadData();
+        this.handleCancel();
         this.tableVisibility = true;
     }
 
-    @wire(getRecords,{objectName: this.selectedValue}) 
-    handleWiredRecords(result) {
-        if(result.data) {
-            this.records = data;
-            this.columns = this.getColumns(data);
-        }else {
-            this.error = result.error;
-            console.log('Error: ' + this.error);
-        }
+    handleChangeDualList(event) {
+        this.selectedFields = event.detail.value;
+        //console.log('handleChangeDualList: ' + JSON.stringify(this.selectedFields));
     }
 
-    getColumns(data) {
-        let columns = [];
-        data.forEach(record => {
-            Object.keys(record).forEach(key => {
-                columns.push({label: key, fieldName: key});
-            });
-        });
-        
-        return columns;
+    getObjectFields() {
+        getFields({objectApiName: this.selectedValue})
+        .then(result => {
+            this.fieldsOptions = result;
+            //console.log('fieldsOptions: ' + JSON.stringify(this.fieldsOptions));
+        })
+        .catch(error => {
+            this.fieldsOptions = [];
+            this.showToast('Error getting fields', error.body.message, 'error');        
+        })
     }
+
+    showToast(title, message, variant) {
+        this.dispatchEvent(new ShowToastEvent({ title, message, variant }));
+    }
+
+    setColoumns (fields) {
+        this.columns = fields.map(field => {
+            //console.log('Mapping label: ' + field.label + ' value: ' + field.value);
+            let fieldName = field;
+            return {
+                label: this.fieldsOptions.find(f => f.value === field).label,       
+                fieldName: field,
+                editable: fieldName == 'Name' ? true : false       
+            };
+        });
+    }
+
+    loadData() {
+        getData({fields: this.selectedFields, objectApiName: this.selectedValue})
+        .then(result => {
+            this.records = result;
+        }) 
+        .catch(error => {
+            this.records = [];
+            this.showToast('Error getting data', error.body.message, 'error')
+        });
+    }
+
 }
